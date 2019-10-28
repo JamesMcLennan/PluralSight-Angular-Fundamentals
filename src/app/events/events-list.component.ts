70e2f1c2be2ae1a420d9b6776e69c8d5bf741ff1
jamesmclennan <jamesamclennan@gmail.com>
2019-10-28 21:38:33 +1100

    Add ngIf statements for variables by using the Save-Navigation operator

--- src/app/events/events-list.component.ts
+++ src/app/events/events-list.component.ts
@@ -92,11 +92,7 @@ export class EventsListComponent {
           time: '9:00 am',
           price: 950.00,
           imageUrl: '/assets/images/ng-nl.png',
-          location: {
-            address: 'The NG-NL Convention Center & Scuba Shop',
-            city: 'Amsterdam',
-            country: 'Netherlands'
-          },
+          onlineUrl: 'www.google.com.au',
           sessions: [
             {
               id: 1,
