ac642f12d48e98674b36d56999b8133a15b5fd2b
jamesmclennan <jamesamclennan@gmail.com>
2019-10-31 07:00:05 +1100

    Added canDeactivate route guard on create event form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -31,7 +31,22 @@ import { EventRouteActivator } from './events/event-details/event-route-activato
     Error404Component
   ],
   // Services
-  providers: [EventService, ToastrService, EventRouteActivator],
+  providers: [
+    EventService, 
+    ToastrService, 
+    EventRouteActivator,
+    { 
+      provide: 'canDeactivateCreateEvent',
+      useValue: checkDirtyState
+    }
+  ],
   bootstrap: [EventsAppComponent]
 })
 export class AppModule { }
+
+export function checkDirtyState(component: CreateEventComponent) {
+  if(component.isDirty) {
+    return window.confirm('You have not saved this event, are you sure you wish to cancel?')
+  }
+  return true
+}
--- src/app/routes.ts
+++ src/app/routes.ts
@@ -6,7 +6,7 @@ import { Error404Component } from './errors/404.component';
 import { EventRouteActivator } from './events/event-details/event-route-activator.service';
 
 export const appRoutes: Routes = [
-  { path: 'events/new', component: CreateEventComponent },
+  { path: 'events/new', component: CreateEventComponent, canDeactivate: ['canDeactivateCreateEvent'] },
   { path: 'events', component: EventsListComponent },
   { path: 'events/:id', component: EventDetailsComponent, canActivate: [EventRouteActivator] },
   { path: '404', component: Error404Component },
