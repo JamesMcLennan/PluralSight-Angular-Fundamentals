e0d02a8f397868538339961535205647d15b07de
jamesmclennan <jamesamclennan@gmail.com>
2019-11-01 07:07:29 +1100

    Completed organisation of imports

--- src/app/events/index.ts
+++ src/app/events/index.ts
@@ -1,2 +1,6 @@
+export * from './events-list.resolver.service';
+export * from './events-list.component';
+export * from './shared/index';
 export * from './create-event/create-event.component';
-export * from'Events.' ;
+export * from './event-details/index';
+export * from './event-thumbnail/event-thumbnail.component';
--- src/app/routes.ts
+++ src/app/routes.ts
@@ -1,10 +1,12 @@
-import {EventsListComponent} from './events/events-list.component';
-import {EventDetailsComponent} from './events/event-details/event-details.component';
+import {
+  EventsListComponent,
+  EventDetailsComponent,
+  CreateEventComponent,
+  EventRouteActivator,
+  EventListResolver
+} from './events/index'
 import {Routes} from '@angular/router';
-import { CreateEventComponent } from './events/create-event/create-event.component';
 import { Error404Component } from './errors/404.component';
-import { EventRouteActivator } from './events/event-details/event-route-activator.service';
-import { EventListResolver } from './events/events-list.resolver.service';
 
 export const appRoutes: Routes = [
   { path: 'events/new', component: CreateEventComponent, canDeactivate: ['canDeactivateCreateEvent'] },
