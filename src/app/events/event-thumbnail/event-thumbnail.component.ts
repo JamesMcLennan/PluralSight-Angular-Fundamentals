d5a1984afbb01b1fc9d6e5abc29d8ef30b6b0587
jamesmclennan <jamesamclennan@gmail.com>
2019-11-02 17:30:37 +1100

    First template-based form

--- src/app/events/event-thumbnail/event-thumbnail.component.ts
+++ src/app/events/event-thumbnail/event-thumbnail.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input } from '@angular/core'
+import { IEvent } from '../shared';
 
 @Component({
     selector: 'event-thumbnail',
@@ -7,7 +8,7 @@ import { Component, Input } from '@angular/core'
 })
 
 export class EventThumbnailComponent {
-    @Input() event: any;
+    @Input() event: IEvent;
 
     getStartTimeClass() {
         const isEarlyStart = this.event && this.event.time === '8:00am';
--- src/app/user/user.module.ts
+++ src/app/user/user.module.ts
@@ -1,16 +1,20 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
 import { userRoutes } from './user.routes';
 import {ProfileComponent} from './profile.component';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   imports: [
     CommonModule,
+    FormsModule,
     RouterModule.forChild(userRoutes)
   ],
   declarations: [
-    ProfileComponent
+    ProfileComponent,
+    LoginComponent
   ],
   providers: [
 
