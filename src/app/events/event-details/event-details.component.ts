aba3d64b5689b1e44d8294f3a48a507b15a93b21
jamesmclennan <jamesamclennan@gmail.com>
2019-11-02 17:13:44 +1100

    Using model for type safety

--- src/app/events/event-details/event-details.component.ts
+++ src/app/events/event-details/event-details.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import {EventService} from '../shared/events.service';
 import {ActivatedRoute} from '@angular/router';
+import { IEvent } from '../shared';
 
 @Component({
   templateUrl: './event-details.component.html',
@@ -8,7 +9,7 @@ import {ActivatedRoute} from '@angular/router';
 })
 
 export class EventDetailsComponent {
-  event: any;
+  event: IEvent;
   constructor(private eventService: EventService, private route: ActivatedRoute) {
 
   }
--- src/app/events/events-list.component.ts
+++ src/app/events/events-list.component.ts
@@ -2,13 +2,14 @@ import { Component, OnInit } from '@angular/core';
 import { EventService } from './shared/events.service';
 import { ToastrService } from '../common/toastr.service';
 import { ActivatedRoute } from '@angular/router';
+import { IEvent } from './shared';
 
 @Component({
     templateUrl: './events-list.component.html'
 })
 
 export class EventsListComponent implements OnInit {
-    events: any;
+    events: IEvent[];
 
     constructor(private eventService: EventService, private toastr: ToastrService, private route: ActivatedRoute ) {
 
