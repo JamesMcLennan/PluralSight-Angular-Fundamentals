ac414bef2a99789e1aa4b6d406804a75c2716a2c
jamesmclennan <jamesamclennan@gmail.com>
2019-11-04 17:16:58 +1100

    Added two-way communication for session adding and cancellation of form

--- src/app/events/event-details/event-details.component.ts
+++ src/app/events/event-details/event-details.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import {EventService} from '../shared/events.service';
 import {ActivatedRoute} from '@angular/router';
-import { IEvent } from '../shared';
+import { IEvent, ISession } from '../shared';
 
 @Component({
   templateUrl: './event-details.component.html',
@@ -10,11 +10,28 @@ import { IEvent } from '../shared';
 
 export class EventDetailsComponent {
   event: IEvent;
+  addMode: boolean;
   constructor(private eventService: EventService, private route: ActivatedRoute) {
 
   }
 
   ngOnInit() {
     this.event = this.eventService.getEvent(+this.route.snapshot.params['id']);
+  }
+
+  addSession() {
+    this.addMode = true
+  }
+
+  saveNewSession(session:ISession) {
+    const nextId = Math.max.apply(null, this.event.sessions.map(s => s.id));
+    session.id = nextId + 1
+    this.event.sessions.push(session)
+    this.eventService.updateEvent(this.event)
+    this.addMode = false
+  }
+
+  cancelAddSession() {
+    this.addMode = false
   }
 }
