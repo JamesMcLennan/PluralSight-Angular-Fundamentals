c2b40d77fb77157bf0c65e135fe4c90c74338fd4
James <jamesamclennan@gmail.com>
2019-11-05 10:42:09 +1100

    Module/data with pipes (#5)
    * Formatted title, date and currency using pipes
    
    * Created a custom duration pipe
    
    * Add filter by buttons to event-details page
    
    * Provide some logic to the session-list component to filter by
    
    * Allow sorting by name or votes

--- src/app/events/event-details/event-details.component.ts
+++ src/app/events/event-details/event-details.component.ts
@@ -11,6 +11,9 @@ import { IEvent, ISession } from '../shared';
 export class EventDetailsComponent {
   event: IEvent;
   addMode: boolean;
+  filterBy: string = 'all';
+  sortBy: string = 'votes';
+
   constructor(private eventService: EventService, private route: ActivatedRoute) {
 
   }
--- src/app/events/event-details/session-list/session-list.component.ts
+++ src/app/events/event-details/session-list/session-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from "@angular/core";
+import { Component, Input, OnChanges } from "@angular/core";
 import { ISession } from '../../shared';
 
 @Component({
@@ -7,6 +7,41 @@ import { ISession } from '../../shared';
     styleUrls: ['./session-list.component.css']
 })
 
-export class SessionListComponent {
+export class SessionListComponent implements OnChanges{
     @Input() sessions:ISession[]
+    @Input() filterBy: string;
+    @Input() sortBy: string;
+    visibleSessions: ISession[] = [];
+
+    ngOnChanges() {
+        if(this.sessions) {
+            this.filterSessions(this.filterBy);
+            this.sortBy === 'name' ? this.visibleSessions.sort(sortByNameAsc) : this.visibleSessions.sort(sortByVotesDesc);
+        }
+    }
+
+    filterSessions(filter) {
+        if(filter == 'all') {
+            // Create a clone  of the sessions variable. 
+            this.visibleSessions = this.sessions.slice(0);
+        } else {
+            this.visibleSessions = this.sessions.filter(session => {
+                return session.level.toLocaleLowerCase() === filter; 
+            })
+        }
+    }
+}
+
+
+function sortByNameAsc(s1: ISession, s2: ISession) {
+    // If Alpha of s1 > s2 return 1
+    if(s1.name > s2.name) return 1
+    // If Alpha is the same return 0
+    else if(s1.name === s2.name) return 0
+    // If Alpha of s1 < s2 return -1
+    else return -1
+}
+
+function sortByVotesDesc(s1: ISession, s2: ISession) {
+    return s2.voters.length - s1.voters.length;
 }
