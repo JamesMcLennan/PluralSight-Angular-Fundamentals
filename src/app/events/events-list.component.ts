9fbbf7e3c63711fb444a08ebfcf3ced2cd0d3ba3
jamesmclennan <jamesamclennan@gmail.com>
2019-11-05 11:32:57 +1100

    Rewrite Toastr service as an InjectionToken

--- src/app/events/events-list.component.ts
+++ src/app/events/events-list.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import { EventService } from './shared/events.service';
-import { ToastrService } from '../common/toastr.service';
 import { ActivatedRoute } from '@angular/router';
 import { IEvent } from './shared';
 
@@ -11,15 +10,11 @@ import { IEvent } from './shared';
 export class EventsListComponent implements OnInit {
     events: IEvent[];
 
-    constructor(private eventService: EventService, private toastr: ToastrService, private route: ActivatedRoute ) {
+    constructor(private eventService: EventService, private route: ActivatedRoute ) {
 
     }
 
     ngOnInit() {
         this.events = this.route.snapshot.data['events']
-    }
-
-    handleThumbnailClick(eventName) {
-        this.toastr.success(eventName);
     }
 }
