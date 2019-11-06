0b1dd0e42000ff7880aa933f3f3fa36b96cf587b
jamesmclennan <jamesamclennan@gmail.com>
2019-11-06 12:20:45 +1100

    Detect and close dialog on click of body

--- src/app/common/simple-modal/simple-modal.component.ts
+++ src/app/common/simple-modal/simple-modal.component.ts
@@ -1,4 +1,5 @@
-import { Component, Input } from "@angular/core";
+import { Component, Input, ViewChild, ElementRef, Inject } from "@angular/core";
+import { JQ_TOKEN } from '../jquery.service';
 
 @Component({
     selector: 'simple-modal',
@@ -9,4 +10,11 @@ import { Component, Input } from "@angular/core";
 export class SimpleModalComponent {
     @Input() title: string;
     @Input() elementId: string;
+    @ViewChild('modalContainer') containerElement: ElementRef;
+
+    constructor(@Inject(JQ_TOKEN) private $:any) {}
+
+    closeModal() {
+        this.$(this.containerElement.nativeElement).modal('hide');
+    }
 }
