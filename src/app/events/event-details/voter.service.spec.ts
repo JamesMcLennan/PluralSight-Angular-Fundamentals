0af661bb66bea2c268e07d18377235e93e75b980
jamesmclennan <jamesamclennan@gmail.com>
2019-11-13 06:44:55 +1100

    Add URL call checks and add voter

--- src/app/events/event-details/voter.service.spec.ts
+++ src/app/events/event-details/voter.service.spec.ts
@@ -20,7 +20,37 @@ describe('VoterService', () => {
 
             expect(session.voters.length).toBe(1);
             expect(session.voters[0]).toBe("john");
-        })
+        });
+
+        it('should call http.delete with the right url', () => {
+            var session = { id: 6, voters: ['joe', 'john']};
+            mockHttp.delete.and.returnValue(of(false));
+
+            voterService.deleteVoter(3, <ISession>session, "joe");
+
+            expect(mockHttp.delete).toHaveBeenCalledWith('/api/events/3/sessions/6/voters/joe');
+        });
+    });
+
+    describe('addVoter', () => {
+        it('should add a voter to the list of voters', () => {
+            var session = { id: 6, voters: ['joe', 'john']};
+            mockHttp.post.and.returnValue(of(false));
+
+            voterService.addVoter(3, <ISession>session, "james");
+
+            expect(session.voters.length).toBe(3);
+            expect(session.voters[2]).toBe("james");
+        });
+
+        it('should call http.post with the right url', () => {
+            var session = { id: 6, voters: ['john']};
+            mockHttp.post.and.returnValue(of(false));
+
+            voterService.addVoter(3, <ISession>session, "joe");
+
+            expect(mockHttp.post).toHaveBeenCalledWith('/api/events/3/sessions/6/voters/joe', {}, jasmine.any(Object));
+        });
     })
 
 });
