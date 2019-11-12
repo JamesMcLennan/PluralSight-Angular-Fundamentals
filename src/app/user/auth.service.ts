2c79d710bd6d5820216de51a4bdbfca8fff20aa1
james-mclennan <james.mclennan@carsales.com.au>
2019-11-13 05:44:01 +1100

    Persisting user data

--- src/app/user/auth.service.ts
+++ src/app/user/auth.service.ts
@@ -28,8 +28,21 @@ export class AuthService {
         return !!this.currentUser;
     }
 
+    checkAuthenticationStatus() {
+        this.http.get('/api/currentIdentity')
+            .pipe(tap(data => {
+                if(data instanceof Object) {
+                    this.currentUser = <IUser>data;
+                }
+            }))
+            .subscribe();
+    }
+
     updateCurrentUser(firstName: string, lastName: string) {
         this.currentUser.firstName = firstName;
         this.currentUser.lastName = lastName;
+        let options = { headers: new HttpHeaders({'Content-Type': 'application/json'})};
+
+        return this.http.put(`/api/users/${this.currentUser.id}`, this.currentUser, options);
     }
 }
--- src/app/user/profile/profile.component.ts
+++ src/app/user/profile/profile.component.ts
@@ -34,7 +34,9 @@ export class ProfileComponent implements OnInit {
   saveProfile(formValues) {
     if(this.profileForm.valid) {
       this.authService.updateCurrentUser(formValues.firstName, formValues.lastName)
-      this.toastr.success("Profile Saved")
+      .subscribe(() => {
+        this.toastr.success("Profile Saved")
+      });
     }
   }
 
