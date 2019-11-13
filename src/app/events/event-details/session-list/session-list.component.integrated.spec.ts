b07d619d9e6ec0c20add7cadcb0d34a1cbc3423c
jamesmclennan <jamesamclennan@gmail.com>
2019-11-14 05:50:00 +1100

    Added intergrated test for checking the initial display title

---
+++ src/app/events/event-details/session-list/session-list.component.integrated.spec.ts
@@ -0,0 +1,72 @@
+import { TestBed, async, ComponentFixture } from '@angular/core/testing';
+import  { DebugElement } from '@angular/core';
+import { By } from '@angular/platform-browser';
+import { SessionListComponent } from './session-list.component';
+import { AuthService } from 'src/app/user/auth.service';
+import { VoterService } from '../voter.service';
+import { UpvoteComponent } from '../upvote/upvote.component';
+import { DurationPipe } from '../../shared';
+import { CollapsibleWellComponent } from 'src/app/common';
+
+describe('SessionListComponent', () => {
+    let fixture: ComponentFixture<SessionListComponent>,
+        component: SessionListComponent,
+        element: HTMLElement,
+        debugEl: DebugElement;
+
+    beforeEach(async(() => {
+        let mockAuthService = {
+            isAuthenticated: () => true,
+            currentUser: { userName: 'Joe' }
+        };
+
+        let mockVoterService = {
+            userHasVoted: () => true
+        };
+
+        TestBed.configureTestingModule({
+            imports: [],
+            declarations: [
+                SessionListComponent,
+                UpvoteComponent,
+                DurationPipe,
+                CollapsibleWellComponent
+            ],
+            providers: [
+                { provide: AuthService, useValue: mockAuthService },
+                { provide: VoterService, useValue: mockVoterService }
+            ],
+            schemas: []
+        });
+    }));
+
+    beforeEach(() => {
+        fixture = TestBed.createComponent(SessionListComponent);
+        component = fixture.componentInstance;
+        debugEl = fixture.debugElement;
+        element = fixture.nativeElement;
+    });
+
+    describe('initial display', () => {
+        it('should have the correct session title', () => {
+            component.sessions = [{
+                id: 3,
+                name: 'Session 1',
+                presenter: 'Joe',
+                duration: 1,
+                level: 'beginner',
+                abstract: 'abstract',
+                voters: ['john', 'bob']
+            }];
+            component.filterBy = 'all';
+            component.sortBy = 'name';
+            component.eventId = 4;
+
+            component.ngOnChanges();
+            fixture.detectChanges();
+
+            //expect(element.querySelector('[well-title]').textContent).toContain('Session 1');
+            expect(debugEl.query(By.css('[well-title]')).nativeElement.textContent).toContain('Session 1');
+        });
+    });
+});
